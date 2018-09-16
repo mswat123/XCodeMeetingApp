145713bb1d31cd323c3575ea3bc6731aba832ea3
Manan Saaraswat <mananswat123@gmail.com>
2018-09-15 23:09:14 -0400

    Initial Commit

---
+++ XCodeMeetingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCodeMeetingApp
+//
+//  Created by Babita saaraswat on 9/15/18.
+//  Copyright © 2018 Manan Saaraswat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
